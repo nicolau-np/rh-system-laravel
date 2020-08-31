3a447696da021453ca9dcb96bf3e14fef15935d7
Nicolau NP <mr1Normaliii@gmail.com>
2020-08-31 21:47:16 +0100

    sms

--- resources/views/relatorio/exports/folha_salario.blade.php
+++ resources/views/relatorio/exports/folha_salario.blade.php
@@ -9,9 +9,9 @@
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+            <title>Folha de Salário</title>
         </head>
-<body style="margin:0px">
+<body>
         Folha de Remunerações<br />
 
 
