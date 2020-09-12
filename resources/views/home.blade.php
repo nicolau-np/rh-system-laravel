759fd37767374a1d3be62e9290a567285137a184
Nicolau NP <mr1Normaliii@gmail.com>
2020-09-12 10:16:17 +0100

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,75 +21,11 @@
     </header>
 
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-12">
             <section class="panel">
-                <header class="panel-heading">
-                    <div class="panel-actions">
-                        <a href="#" class="fa fa-caret-down"></a>
-                        <a href="#" class="fa fa-times"></a>
-                    </div>
-
-                    <h2 class="panel-title">Best Seller</h2>
-                    <p class="panel-subtitle">Customize the graphs as much as you want, there are so many options and features to display information using JSOFT Admin Template.</p>
-                </header>
+               
                 <div class="panel-body">
-
-                    <!-- Flot: Basic -->
-                    <div class="chart chart-md" id="flotDashBasic"></div>
-                    <script>
-                        var flotDashBasicData = [{
-                            data: [
-                                [0, 170],
-                                [1, 169],
-                                [2, 173],
-                                [3, 188],
-                                [4, 147],
-                                [5, 113],
-                                [6, 128],
-                                [7, 169],
-                                [8, 173],
-                                [9, 128],
-                                [10, 128]
-                            ],
-                            label: "Series 1",
-                            color: "#0088cc"
-                        }, {
-                            data: [
-                                [0, 115],
-                                [1, 124],
-                                [2, 114],
-                                [3, 121],
-                                [4, 115],
-                                [5, 83],
-                                [6, 102],
-                                [7, 148],
-                                [8, 147],
-                                [9, 103],
-                                [10, 113]
-                            ],
-                            label: "Series 2",
-                            color: "#2baab1"
-                        }, {
-                            data: [
-                                [0, 70],
-                                [1, 69],
-                                [2, 73],
-                                [3, 88],
-                                [4, 47],
-                                [5, 13],
-                                [6, 28],
-                                [7, 69],
-                                [8, 73],
-                                [9, 28],
-                                [10, 28]
-                            ],
-                            label: "Series 3",
-                            color: "#734ba9"
-                        }];
-
-                        // See: {{asset('assets/javascripts/dashboard/examples.dashboard.js')}} for more settings.
-                    </script>
-
+ola
                 </div>
             </section>
         </div>
